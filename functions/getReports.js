be39d18fd665749a6bea65bbc2dacc01f4a71ba2
joeljtomy <122166595+joeljtomy@users.noreply.github.com>
2024-07-28 19:16:09 +0530

    minor change

--- functions/getReports.js
+++ functions/getReports.js
@@ -3,11 +3,6 @@ const Report = require("../schemas/report");
 async function getReports(videoIds) {
   try {
     const reports = await Report.find({ id: { $in: videoIds } });
-    videoIds.forEach((id) => {
-      if (!reports.map((item) => item.id).includes(id)) {
-        reports.push({ id, count: 0 });
-      }
-    });
     return reports;
   } catch (error) {
     console.log("Error: ", error);
