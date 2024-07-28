f28046c9eff887b517c4d82aaabb4247443ba518
joeljtomy <122166595+joeljtomy@users.noreply.github.com>
2024-07-28 09:56:37 +0530

    environment setup, cleanup

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`Server ready on port ${port}.`));
@@ -9,3 +10,5 @@ app.get("/", (req, res) => {
     connected: true,
   });
 });
+
+module.exports = app;
